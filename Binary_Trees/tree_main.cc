acc99321acb4ba832e87d78c57a2151777d47632
rshukla3 <rshukla3@wisc.edu>
2014-07-05 14:39:21 -0500

    An updated code for tree traversal. Currently this code is able to read a text file that will be later required to form the tree before the traversal begins.

--- Binary_Trees/tree_main.cc
+++ Binary_Trees/tree_main.cc
@@ -1,11 +1,46 @@
 #include<stdio.h>
 #include "tree_class.hh"
+#include "string.h"
 using namespace std;
+
+int create_tree(int *);
+
 int main()
 {
     tree T;
-     
+    int *h; 
     printf("This is a test by rohit\n");
+    int ret = create_tree(h);
     return 0;
 }
 
+
+int create_tree(int *head)
+{
+
+    FILE *fp;
+    char c;
+    char *buffer;
+    int i;
+    fp = fopen("tree_node_values", "r");
+    fseek(fp, SEEK_SET, 0);
+    i = 0;
+    while(1){
+	c = fgetc(fp);
+	if(feof(fp)){
+	    break;
+	}
+	if(c != '\n'){
+	    buffer[i] = c;
+	    i++;
+	}
+    }
+    //fread(buffer, sizeof(char), 14, fp);
+    printf("The read string is:\n");
+    for(i = 0; i < 7; i++)
+    {
+	printf("%c\n", buffer[i]);
+    }
+    fclose(fp);
+    return 0; 
+}
