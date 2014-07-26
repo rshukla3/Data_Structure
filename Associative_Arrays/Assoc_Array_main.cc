4feb1702bed1fc4118cbe81fe66737940e853e95
rshukla3 <rshukla3@wisc.edu>
2014-07-25 23:14:16 -0500

    Learned how to implement reverse iterator for maps, how to write template class code in a separate cpp or cc file, and a new headerfile limits to get the maximum minimum and different functions for numeric datatypes.

--- Associative_Arrays/Assoc_Array_main.cc
+++ Associative_Arrays/Assoc_Array_main.cc
@@ -1,18 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<limits>
+
 #include"Map_STL.hh"
+
+#define MAX_KEY_VAL 10
+#define DATA_RANGE 1000
 using namespace std;
 
 int main(){
     map<int, int> m;
     maps<int, int> m_int;
-    //maps<float, float> m_float;
-    int a = 6;
-    int b = 2;
-    //float A_f = 16.573;
-    //float B_f = 16.577;
-    m_int.push_value(a,b,m);
-    //m_float.push_value(A_f, B_f);
+    int a;
+    int b;
+    int i;
 
-    printf("Started working on associative arrays m[%d]=%d\n",a,b);
+    for(i = 0; i<MAX_KEY_VAL; ++i){
+	a=rand()%DATA_RANGE+100;
+	b=rand()%DATA_RANGE+a;
+	m_int.push_value(a,b,m);
+    }
+
+    map<int, int>::reverse_iterator it;
+    
+    for(it=m.rbegin(); it!=m.rend();++it){
+	printf("Started working on associative arrays m[%d]=%d\n",it->first,it->second);
+    }
+    i = numeric_limits<int>::max();
+    cout<<"The maximum value of integer is:"<<numeric_limits<int>::max()<<endl;
+    cout<<"The minimum value of integer is:"<<numeric_limits<int>::min()<<endl;
+
+    cout<<"The integer division value is:"<<i/10<<endl;
     return 0;
 }
